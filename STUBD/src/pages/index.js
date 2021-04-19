26b6552ac3013a96d18bdc614f59a1fa2c2fa78c
claudemahon <claudemahonjr@gmail.com>
2021-04-18 21:05:58 -0400

    small changes

--- STUBD/src/pages/index.js
+++ STUBD/src/pages/index.js
@@ -4,10 +4,11 @@ import "./style.css"
 
 const IndexPage = () => (
 <>
-<section className="hero is-fullheight">
+<section className="hero is-fullheight has-text-centered">
   <div className="hero-body bg-img">
     {/* main view */}
   </div>
+  <p className="subtitle">Comming Soon</p>
 </section>
 </>
 )
